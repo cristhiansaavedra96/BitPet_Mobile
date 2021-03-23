3d6f50b2f68850cffba7aace6d6aeb5acf8cc93d
Cristhian Saavedra <cristhiansaavedra1996@gmail.com>
2021-03-23 20:07:05 -0300

    Primer Commit

---
+++ src/main/java/com/example/bitpet_mobile/PanelPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.bitpet_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PanelPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panel_principal);
+    }
+}
